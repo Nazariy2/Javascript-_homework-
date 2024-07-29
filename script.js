405ebe32c473610fc27cf4164c9fe92f4f4dfe4d
Nazariy2 <nazrud23@gmail.com>
2024-07-29 13:04:22 +0200

    a

---
+++ script.js
@@ -0,0 +1,42 @@
+// let cels = prompt("write ur temperature");
+// let far =  (cels * 9/5) + 32;
+
+// console.log(`${cels}°C is ${far}°F`)
+
+// let daysInMonth = prompt("write the number of days u want ");
+// let hoursInMonth = daysInMonth * 24;
+// let minutesInMonth = daysInMonth * 24 * 60;
+
+// console.log(`In ${daysInMonth} days there are ${hoursInMonth} hours and ${minutesInMonth} minutes.`);
+
+// let characterLife = 100;
+// let characterEnergy = 50;
+// let enemy = characterLife - 25;
+// let sprint = characterEnergy - 30;
+
+// console.log(`Character life at the start of the game is ${characterLife} and his energy is ${characterEnergy} after figthing an enemy characters life is ${enemy} and his energy after the duel is ${sprint}`);
+
+// let price = prompt("write ur price for 10% discount");
+// let discount = price * 10 / 100;
+// let finalPrice = price - discount;
+
+// console.log(`Ur price is ${price}$ with the 10% discoutn ur final price is ${finalPrice}$`);
+
+// let floatNumber = prompt("write a number");
+// let roundedNumber = Math.floor(floatNumber);
+
+// console.log(`rounded number: ${roundedNumber}`);
+
+
+let stringWithFloat = "123.456";
+let floatNumber = parseFloat(stringWithFloat);
+
+console.log(`Numeber: ${floatNumber}`);
+
+
+
+let string = "12345";
+let number = parseInt(string, 10);
+
+
+console.log(`Number rounded: ${number}`);
